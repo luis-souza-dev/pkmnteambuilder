d33d3c287785f00e32819f3c59c4131c5241dfdb
Luis Souza <luisotavioldesouza1+1@gmail.com>
2023-03-02 18:32:38 -0300

    build: initing repo

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  let a: string;
+  
+
+  return (
+    <div className="">
+    </div>
+  )
+}
+
+export default App
